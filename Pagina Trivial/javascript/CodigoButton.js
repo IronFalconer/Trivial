dfd58af34e28aed79ad666e4a6fb0bf618761c37
IronFalconer <82568775+IronFalconer@users.noreply.github.com>
2021-04-29 01:04:22 +0200

    Add files via upload

---
+++ Pagina Trivial/javascript/CodigoButton.js
@@ -0,0 +1,315 @@
+ //Pregunta 1
+function FbotonOn1() {
+    var uno = document.getElementById('button1.1');
+    if (uno.innerHTML == 'Correcto') 
+      uno.innerHTML = 'respuesta a)';
+    else uno.innerHTML = 'Correcto'; 
+}
+function FbotonOn2() {
+    var uno = document.getElementById('button2.1');
+    if (uno.innerHTML == 'Incorrecto') 
+      uno.innerHTML = 'respuesta b)';
+    else uno.innerHTML = 'Incorrecto'; 
+}
+function FbotonOn3() {
+    var uno = document.getElementById('button3.1');
+    if (uno.innerHTML == 'Incorrecto') 
+      uno.innerHTML = 'respuesta c)';
+    else uno.innerHTML = 'Incorrecto'; 
+}
+$(function () { //nos asegura que el dom ya está creado
+    
+   
+    //Pregunta 2
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 3
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 4
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 5
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 6
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    //Pregunta 7
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 8
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 9
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    //Pregunta 10
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 11
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 12
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 13
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 14
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    //Pregunta 15
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 16
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    //Pregunta 17
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 18
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    //Pregunta 19
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 20
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    //Pregunta 21
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 22
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 23
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 24
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 25
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 26
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    //Pregunta 27
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 28
+    $("#button1").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 29
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("Correctoo!!")
+    });
+    $("#button3").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    //Pregunta 30
+    $("#button1").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button2").on("click", function () { 
+        alert("ErrOrrrRr")
+    });
+    $("#button3").on("click", function () { 
+        alert("Correctoo!!")
+    });
+
+
+})
