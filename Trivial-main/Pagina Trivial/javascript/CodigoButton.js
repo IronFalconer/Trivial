fc3032bf6e56f9717df5f58d211894a090f2fa2f
IronFalconer <82568775+IronFalconer@users.noreply.github.com>
2021-04-29 09:19:59 +0200

    Add files via upload

---
+++ Trivial-main/Pagina Trivial/javascript/CodigoButton.js
@@ -0,0 +1,59 @@
+
+//Pregunta op1
+function boton1() {
+    var uno = document.getElementById('button1');
+    if (uno.innerHTML == 'Correcto') 
+      uno.innerHTML = 'respuesta a)';
+    else uno.innerHTML = 'Correcto'; 
+}
+function boton2() {
+    var uno = document.getElementById('button2');
+    if (uno.innerHTML == 'Incorrecto') 
+      uno.innerHTML = 'respuesta b)';
+    else uno.innerHTML = 'Incorrecto'; 
+}
+function boton3() {
+    var uno = document.getElementById('button3');
+    if (uno.innerHTML == 'Incorrecto') 
+      uno.innerHTML = 'respuesta c)';
+    else uno.innerHTML = 'Incorrecto'; 
+}
+//Pregunta op2
+function boton4() {
+    var uno = document.getElementById('button4');
+    if (uno.innerHTML == 'Incorrecto') 
+      uno.innerHTML = 'respuesta a)';
+    else uno.innerHTML = 'Incorrecto'; 
+}
+function boton5() {
+    var uno = document.getElementById('button5');
+    if (uno.innerHTML == 'Correcto') 
+      uno.innerHTML = 'respuesta b)';
+    else uno.innerHTML = 'Correcto'; 
+}
+function boton6() {
+    var uno = document.getElementById('button6');
+    if (uno.innerHTML == 'Incorrecto') 
+      uno.innerHTML = 'respuesta c)';
+    else uno.innerHTML = 'Incorrecto'; 
+}
+//Pregunta op3
+function boton7() {
+    var uno = document.getElementById('button7');
+    if (uno.innerHTML == 'Incorrecto') 
+      uno.innerHTML = 'respuesta a)';
+    else uno.innerHTML = 'Incorrecto'; 
+}
+function boton8() {
+    var uno = document.getElementById('button8');
+    if (uno.innerHTML == 'Incorrecto') 
+      uno.innerHTML = 'respuesta b)';
+    else uno.innerHTML = 'Incorrecto'; 
+}
+function boton9() {
+    var uno = document.getElementById('button9');
+    if (uno.innerHTML == 'Correcto') 
+      uno.innerHTML = 'respuesta c)';
+    else uno.innerHTML = 'Correcto'; 
+}
+
